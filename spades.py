01b956c41f80a490e9adedd7a6a1f025798efc19
causticcutlery <50019394+causticcutlery@users.noreply.github.com>
2022-07-29 21:55:22 -0400

    Update spades.py

--- spades.py
+++ spades.py
@@ -10,102 +10,125 @@ deck = ['SB', 'SL', 'S2', 'SA', 'SK', 'SQ', 'SJ', 'ST', 'S9', 'S8', 'S7', 'S6', 
 #Value is calculated on subjective values
 #TODO: create a more objective rating system based on the chance a card takes a book
 def count_value(hand):
-  value=0
+    value=0
   
-  countSpades = (len([card for card in hand if card[0]=="S"]))
-  countHearts = (len([card for card in hand if card[0]=="H"]))
-  countClubs = (len([card for card in hand if card[0]=="C"]))
-  countDiamonds = (len([card for card in hand if card[0]=="D"]))
-  countSuits = [['S','H','C','D'],[countSpades,countHearts,countClubs,countDiamonds]]
+    countSpades = (len([card for card in hand if card[0]=="S"]))
+    countHearts = (len([card for card in hand if card[0]=="H"]))
+    countClubs = (len([card for card in hand if card[0]=="C"]))
+    countDiamonds = (len([card for card in hand if card[0]=="D"]))
+    countSuits = [['S','H','C','D'],[countSpades,countHearts,countClubs,countDiamonds]]
   
-  for card in hand:
-    #High spades are worth the most points
-    if(card == "SB"):
-      value+=6
-    elif(card == "SL"):
-      value+=5
-    elif(card == "S2"):
-      value+=4
-    elif(card == "SA"):
-      value+=3
-    elif(card == "SK"):
-      value+=2
-    elif(card == "SQ"):
-      value+=2
-    #Accounts for the fact that high spades are worth less if it is the only one
-    #This is due to the inability to protect a spade from being drawn out by a higher spade
-    if(countSpades==1 and card != "SB"):
-      value-=2
+    for card in hand:
+        #High spades are worth the most points
+        if(card == "SB"):
+            value+=6
+        elif(card == "SL"):
+            value+=5
+        elif(card == "S2"):
+            value+=4
+        elif(card == "SA"):
+            value+=3
+        elif(card == "SK"):
+            value+=2
+        elif(card == "SQ"):
+            value+=2
+        #Accounts for the fact that high spades are worth less if it is the only one
+        #This is due to the inability to protect a spade from being drawn out by a higher spade
+        if(countSpades==1 and card != "SB"):
+            value-=2
+          
+        #Value of an H/C/D A is normally worth 4
+        #Value goes down by 1 for every additional card in the same suit over 3
+        #This is because the more of a suit a hand has, the more likely it is for another player to cut with spades
+        elif(card[1] == "A"):
+            value+= 4-(countSuits[1][countSuits[0].index(card[0])]-3)
+          '''
+          if(card[0]=="H" and countHearts < 5):
+            value+=3
+          elif(card[0]=="C" and countHearts < 5):
+            value+=3
+          elif(card[0]=="D" and countHearts < 5):
+            value+=3
+          else:
+            value+=2
+          if(card[0]=="H" and countHearts < 5):
+            value+=3
+          elif(card[0]=="C" and countHearts < 5):
+            value+=3
+          elif(card[0]=="D" and countHearts < 5):
+            value+=3
+          else:
+            value+=2
+          if(card[0]=="H" and countClubs < 5):
+            value+=3
+          elif(card[0]=="C" and countClubs < 5):
+            value+=3
+          elif(card[0]=="D" and countClubs < 5):
+            value+=3
+          else:
+            value+=2 
+          '''
+        #Value of an H/C/D K is normally worth 3
+        #Value goes down by 1 for every additional card in the same suit over 2
+        elif(card[1] == "K"):
+            value+= 3-(countSuits[1][countSuits[0].index(card[0])]-2)
+        #Value of an H/C/D Q is normally worth 2
+        #Value goes down by 1 for every additional card in the same suit over 1
+        elif(card[1] == "Q"):      
+            value+= 2-(countSuits[1][countSuits[0].index(card[0])]-1)  
+        
+        #Hands that are void of H,C,D are worth 3 more if there are spades in the hand
+        #Value increase is reduced by 1 for each card of that suit, zeroing off at 3 or more of a suit
+        #This is due to the likelihood of cutting that suit with spades
+        if(countHearts < 3 or countClubs < 3 or countDiamonds < 3):
+            if(countSpades >= (3 - countHearts)):
+                value += (3 - countHearts)
+            else:
+                value += (countSpades+1)
+            if(countSpades >= (3 - countClubs)):
+                value += (3 - countClubs)
+            else:
+                value += (countSpades+1)        
+            if(countSpades >= (3 - countDiamonds)):
+                value += (3 - countDiamonds)
+            else:
+                value += (countSpades+1)        
+          
+        #If a hand is dealt no spades, the player can call a miseal
+        if(countSpades == 0):
+            misdeal = True
       
-    #Value of an H/C/D A is normally worth 4
-    #Value goes down by 1 for every additional card in the same suit over 3
-    #This is because the more of a suit a hand has, the more likely it is for another player to cut with spades
-    elif(card[1] == "A"):
-      value+= 4-(countSuits[1][countSuits[0].index(card[0])]-3)
-      '''
-      if(card[0]=="H" and countHearts < 5):
-        value+=3
-      elif(card[0]=="C" and countHearts < 5):
-        value+=3
-      elif(card[0]=="D" and countHearts < 5):
-        value+=3
-      else:
-        value+=2
-      if(card[0]=="H" and countHearts < 5):
-        value+=3
-      elif(card[0]=="C" and countHearts < 5):
-        value+=3
-      elif(card[0]=="D" and countHearts < 5):
-        value+=3
-      else:
-        value+=2
-      if(card[0]=="H" and countClubs < 5):
-        value+=3
-      elif(card[0]=="C" and countClubs < 5):
-        value+=3
-      elif(card[0]=="D" and countClubs < 5):
-        value+=3
-      else:
-        value+=2 
-      '''
-    #Value of an H/C/D K is normally worth 3
-    #Value goes down by 1 for every additional card in the same suit over 2
-    elif(card[1] == "K"):
-      value+= 3-(countSuits[1][countSuits[0].index(card[0])]-2)
-    #Value of an H/C/D Q is normally worth 2
-    #Value goes down by 1 for every additional card in the same suit over 1
-    elif(card[1] == "Q"):      
-      value+= 2-(countSuits[1][countSuits[0].index(card[0])]-1)  
+def deal_hand(deck):
+    playerNorth = []
+    playerEast = []
+    playerSouth = []
+    playerWest = []
+  
+    for i in range (len(deck)):
+        if(i%4 == 0):
+          playerNorth.append(deck[i])
+        elif(i%4 == 1):
+          playerEast.append(deck[i])
+        elif(i%4 == 2):
+          playerSouth.append(deck[i])
+        elif(i%4 == 3):
+          playerWest.append(deck[i])      
     
-    #Hands that are void of H,C,D are worth 3 more if there are spades in the hand
-    #Value increase is reduced by 1 for each card of that suit, zeroing off at 3 or more of a suit
-    #This is due to the likelihood of cutting that suit with spades
-    if(countHearts < 3 or countClubs < 3 or countDiamonds < 3):
-      if(countSpades >= (3 - countHearts)):
-        value += (3 - countHearts)
-      else:
-        value += (countSpades+1)
-      if(countSpades >= (3 - countClubs)):
-        value += (3 - countClubs)
-      else:
-        value += (countSpades+1)        
-      if(countSpades >= (3 - countDiamonds)):
-        value += (3 - countDiamonds)
-      else:
-        value += (countSpades+1)        
+    playerNorth.sort()
+    playerEast.sort()
+    playerSouth.sort()
+    playerWest.sort()
       
-    #If a hand is dealt no spades, the player can call a miseal
-    if(countSpades == 0):
-      misdeal = True
+    return [playerNorth, playerEast, playerSouth, playerWest]
+  
+  
+dealtCards = deal_hand(deck)
+playerNorth = dealtCards[0]
+playerEast = dealtCards[1]
+playerSouth = dealtCards[2]
+playerWest = dealtCards[3]
       
-      
-      
-      
-      
-      
-      
-      
-      
+print(dealtCards)
       
       
       
